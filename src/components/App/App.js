2b42fec5953507a5ff27a7ced0ffcd68f0ca30ac
Vel Mor <lydjmor@gmail.com>
2020-04-17 15:09:15 -0500

    added genres page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import Home from '../pages/Home/Home'
 import Movies from '../pages/Movies/Movies';
 import Details from '../pages/Details/Details';
+import Genres from '../pages/Genres/Genres';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -16,12 +17,14 @@ class App extends Component {
         <header>
         <h1>Let's Watch A Movie</h1>
         </header>
+        <nav href="/genres">Genres</nav>
       </div>
 
       <Router>
         <Route exact path='/' component={Home} />
         <Route path='/movies' component={Movies} />
         <Route path='/details' component={Details} />
+        <Route path='/genres' component ={Genres}/>
       </Router>
     </>
     );
---
+++ src/components/pages/Genres/Genres.js
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+import React, { Component } from 'react';
+
+class Genres extends Component {
+    
+    render() {
+    return (
+        <>
+        
+        </>
+    );
+  }
+}
+
+export default connect()(Genres);
